b435037bb14ac770bfb8f2243fdada2fb926920d
Paloma <paloma.a.silva@capgemini.com>
2021-09-03 20:37:48 -0300

    API REST controle de Ponto

---
+++ README.md
@@ -0,0 +1,5 @@
+# Gerenciamento de Ponto Api-Rest
+
+Api rest para gerenciamento de ponto e controle de acesso.
+
+Java | Spring boot | Spring Data Jpa | Hibernate | Lombok | swagger
---
+++ src/main/java/com/paloma/controledeponto/repository/JornadaRepository.java
@@ -0,0 +1,9 @@
+package com.paloma.controledeponto.repository;
+
+import com.paloma.controledeponto.model.JornadaTrabalho;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JornadaRepository extends JpaRepository<JornadaTrabalho, Long> {
+}
